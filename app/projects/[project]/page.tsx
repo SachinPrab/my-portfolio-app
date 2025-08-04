db1446b2beb69bd02b869bbe1ca913f3aa753b6f
Sachin <sachprab543@gmail.com>
2025-08-04 17:49:08 +0530

    created type props to assign to params as type

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -2,7 +2,13 @@ import { getProject } from '@/sanity/schemas/sanity-utils';
 import { PortableText } from '@portabletext/react';
 import Image from 'next/image';
 
-export default async function Project({ params }: { params: { project: string } }) {
+type Props = {
+  params: {
+    project: string;
+  };
+};
+
+export default async function Project({ params }: Props ) {
   const slug = params.project;
   const project = await getProject(slug);
 
